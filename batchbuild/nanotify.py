1297182354652e30a64a0bd8d8250234d0ef843f
Aurélien Gâteau <agateau@kde.org>
2013-09-09 10:58:41 +0200

    Catch more dbus errors
    Got an exception in get_object(), let's handle any failure

--- batchbuild/nanotify.py
+++ batchbuild/nanotify.py
@@ -3,17 +3,16 @@ import dbus
 def notify(title, body, icon=""):
     try:
         bus = dbus.SessionBus()
+        obj = bus.get_object('org.freedesktop.Notifications', '/org/freedesktop/Notifications')
+        iface = dbus.Interface(obj, dbus_interface = 'org.freedesktop.Notifications')
+        iface.Notify("nanotify",
+            0,
+            icon,
+            title,
+            body,
+            [], # actions
+            {}, # hints
+            -1, # timeout
+            )
     except dbus.exceptions.DBusException:
         return
-    obj = bus.get_object('org.freedesktop.Notifications', '/org/freedesktop/Notifications')
-    iface = dbus.Interface(obj, dbus_interface = 'org.freedesktop.Notifications')
-    iface.Notify("nanotify",
-        0,
-        icon,
-        title,
-        body,
-        [], # actions
-        {}, # hints
-        -1, # timeout       
-        )
-
